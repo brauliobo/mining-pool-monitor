ff08a235590974e040d1c3c643db7c2fad8f64d8
Braulio Bhavamitra <brauliobo@gmail.com>
2021-05-13 15:01:54 -0300

    views: implement longer periods

--- views.sql
+++ views.sql
@@ -1,77 +1,47 @@
-drop view if exists recent_points;
-create view recent_points as 
-  select
-    pool,
-    wallet,
-    balance,
-    reported_hashrate,
-    read_at,
-    row_number() over (partition by pool,wallet order by read_at asc) as row
-  from wallets
-  where reported_hashrate > 0
-    and balance > 0
-    and read_at > datetime('now', '-48 hour');
-
-drop view if exists balances;
-create view balances as 
-  select
+drop view if exists periods;
+create view periods as
+ select
     p.pool,
     p.wallet,
-    round((julianday(p2.read_at) - julianday(p.read_at)) * 24, 2) as hours,
-    round((p.reported_hashrate + p2.reported_hashrate) / 2, 2) as average_hashrate,
+	round((julianday(p2.read_at) - julianday(p.read_at)) * 24, 2) as hours,
+    round((julianday(p2.read_at) - julianday(p.read_at)) * 24 / 12) * 12 as period,
+    round((p.reported_hashrate + p2.reported_hashrate) / 2, 2) as hashrate,
+	p2.balance - p.balance as reward,
     round(p.reported_hashrate, 2)  as first_hashrate,
     p.balance  as first_balance,
     p.read_at  as first_read_at,
     round(p2.reported_hashrate, 2) as second_hashrate,
     p2.balance as second_balance,
     p2.read_at as second_read_at
-  from recent_points p
-  join recent_points p2 on p2.pool = p.pool and p2.wallet = p.wallet
-   and p.row == 1 and p2.row > p.row
-  where second_balance > first_balance 
-    and 100 * abs(p2.reported_hashrate - p.reported_hashrate)/p.reported_hashrate < 5
-  group by p.pool, p.wallet, hours;
+  from wallets p
+  join wallets p2 on p2.pool = p.pool and p2.wallet = p.wallet and p2.read_at > p.read_at
+  where first_read_at > datetime('now', '-80 hour') and second_read_at > datetime('now', '-6 hour')
+    and hours >= 10 and period in (12,24,48,72)
+    and second_balance > first_balance
+    and 5 > 100 * abs(p2.reported_hashrate - p.reported_hashrate)/p.reported_hashrate
+  group by p.pool, p.wallet, period having max(p2.read_at)
+  order by p.pool, p.wallet, period desc;
 
 drop view if exists rewards;
-create view rewards as 
+create view rewards as
   select
     pool,
-    wallet,
-    second_read_at as last_read_at,
-    hours,
-    average_hashrate as hashrate,
-    second_balance - first_balance as reward
-  from balances
-  group by pool, wallet, hours;
-
-drop view if exists readings;
-create view readings as 
-  select
-    pool,
-    wallet,
-    hashrate,
-    last_read_at,
-    hours,
-    (24 / hours) * (reward / hashrate) as eth_mh_day
-  from rewards
-  group by pool, wallet, hours;
-
-drop view if exists last_readings;
-create view last_readings as 
-  select 
-    r.*
-  from readings r
-  left join readings r2 on r2.pool = r.pool and r2.wallet = r.wallet and r2.last_read_at > r.last_read_at
-  where r2.last_read_at is null;
+    round(avg(hours),2) as hours,
+    period,
+    (24 / avg(hours)) * (avg(reward) / avg(hashrate)) as eth_mh_day
+  from periods
+  group by pool, period;
 
 drop view if exists pools;
-create view pools as 
-  select 
+create view pools as
+  select
     pool,
-    round(avg(eth_mh_day), 8) as 'ETH/MH/DAY',
-    round(avg(hours), 2) as hours
-  from last_readings
-  where hours > 6
+	round(avg(case when period = 24 then eth_mh_day end), 7) as `12h`,
+	round(avg(case when period = 24 then eth_mh_day end), 7) as `24h`,
+	round(avg(case when period = 48 then eth_mh_day end), 7) as `48h`,
+	round(avg(case when period = 72 then eth_mh_day end), 7) as `72h`
+  from rewards
   group by pool
-  order by round(avg(eth_mh_day), 8) desc;
+  order by `72h` desc;
 
+select * from pools
