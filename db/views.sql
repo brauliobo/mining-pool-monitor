f8cc746bbc1c6c153d6b3560a1fe79ac418e5fa4
Braulio Bhavamitra <brauliobo@gmail.com>
2021-05-21 21:32:54 -0300

    db/pools: add pool rank numbers

--- db/views.sql
+++ db/views.sql
@@ -65,14 +65,15 @@ order by pool, wallet, id.period;
 drop view if exists pools;
 create view pools as
 select
-  pool,
+  row_number() over(order by avg(case when period = 216 then eth_mh_day end) desc) || '. ' || pool as pool,
   round(avg(case when period = 24  then eth_mh_day::numeric end), 2) as "1d",
   round(avg(case when period = 72  then eth_mh_day::numeric end), 2) as "3d",
   round(avg(case when period = 144 then eth_mh_day::numeric end), 2) as "6d",
   round(avg(case when period = 216 then eth_mh_day::numeric end), 2) as "9d"
 from rewards
 group by pool
-order by "9d" desc nulls last, "6d" desc nulls last, "3d" desc nulls last, "1d" desc nulls last;
+order by pool;
+
 
 
 
