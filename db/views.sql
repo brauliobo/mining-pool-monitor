43dcf276b91523732d5023491cfe1c9c5b7aa734
Braulio Bhavamitra <brauliobo@gmail.com>
2021-05-21 21:39:16 -0300

    db/pools: add missing nulls last

--- db/views.sql
+++ db/views.sql
@@ -65,7 +65,7 @@ order by pool, wallet, id.period;
 drop view if exists pools;
 create view pools as
 select
-  row_number() over(order by avg(case when period = 216 then eth_mh_day end) desc) || '. ' || pool as pool,
+  row_number() over(order by avg(case when period = 216 then eth_mh_day end) desc nulls last) || '. ' || pool as pool,
   round(avg(case when period = 24  then eth_mh_day::numeric end), 2) as "1d",
   round(avg(case when period = 72  then eth_mh_day::numeric end), 2) as "3d",
   round(avg(case when period = 144 then eth_mh_day::numeric end), 2) as "6d",
