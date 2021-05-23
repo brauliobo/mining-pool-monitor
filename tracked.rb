9c3d66da35365b819dbdc63d025201ae6f5a7b14
Braulio Bhavamitra <brauliobo@gmail.com>
2021-05-22 22:54:36 -0300

    tracked: only update last_read_at if hashrate is valid

--- tracked.rb
+++ tracked.rb
@@ -2,13 +2,13 @@ class Tracked
 
   def self.track data
     update = {hashrate_last: :excluded__hashrate_last, last_read_at: :excluded__last_read_at}
-    record = {
+    record = SymMash.new(
       coin:          data.coin,
       pool:          data.pool,
       wallet:        data.wallet,
       hashrate_last: data.hashrate,
-      last_read_at:  Time.now,
-    }
+    )
+    record.last_read_at = Time.now if data.hashrate > 0
 
     DB[:wallets_tracked]
       .insert_conflict(constraint: :wallets_tracked_unique_constraint, update: update)
