a9dc5efac50862c37e304a0cc7fd8efd50873d3d
Braulio Bhavamitra <brauliobo@gmail.com>
2021-05-24 20:59:15 -0300

    bot/db: change period to weeks

--- db/interval_defs.sql
+++ db/interval_defs.sql
@@ -1,2 +1,3 @@
 delete from intervals_defs;
-insert into intervals_defs(period,label,seq) values (24,'1d',1),(72,'3d',3),(144,'6d',6),(216,'9d',9);
+insert into intervals_defs(period,label,seq) values (24,'24h',1),(72,'3d',3),(144,'7d',6),(216,'14d',9);
+
