b23417fe8dba58238d30b0895b0f7a61459ac051
Braulio Bhavamitra <brauliobo@gmail.com>
2021-05-24 12:37:37 -0300

    db: remove repeated section on order by for partition by, no change in results

--- db/views.sql
+++ db/views.sql
@@ -37,7 +37,7 @@ FROM (
   select
     row_number() over(
       partition by wp.pool, wp.wallet, iseq
-      order by wp.pool, wp.wallet, iseq, second_read desc, abs(hours / period - 1) asc) as row,
+      order by second_read desc, abs(hours / period - 1) asc) as row,
     wp.*
   from wallet_pairs wp
 ) wp
