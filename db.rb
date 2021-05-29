b6379600979ddae058c8e297540b0203fb45aeb3
Braulio Bhavamitra <brauliobo@gmail.com>
2021-05-29 15:52:37 -0300

    db/interval_defs: fix seq

--- db.rb
+++ db.rb
@@ -6,10 +6,10 @@ DB = Sequel.connect adapter: 'postgres', database: 'mining_pools'
 Sequel.split_symbols = true
 Sequel.extension :core_extensions
 
-DB.run File.read ARGV[0] if ARGV[0] and File.exists? ARGV[0]
-
 if ENV['DEBUG']
   DB.sql_log_level = :debug
   DB.loggers << Logger.new($stdout)
 end
+
+DB.run File.read ARGV[0] if ARGV[0] and File.exists? ARGV[0]
 
