7ffc00a93d69b18c8a567be5d8f3d852b1427642
Braulio Bhavamitra <brauliobo@gmail.com>
2021-06-04 23:17:05 -0300

    db/cleanup: prevent deletion of old data

--- db/cleanup.sql
+++ db/cleanup.sql
@@ -4,7 +4,7 @@ select
 from wallet_reads wr
 left join filtered_wallet_pairs wp on wp.pool = wr.pool and wp.wallet = wr.wallet and (wp.first_read = wr.read_at or wp.second_read = wr.read_at)
 where wp.pool is null
-  and wr.read_at <= now() - '48 hours'::interval
+  and (wr.read_at >= (select min(start_date) from intervals) and wr.read_at <= now() - '48 hours'::interval)
 );
 
 refresh materialized view periods_materialized;
