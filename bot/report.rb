fdf822d3f571d5b2e7825bb0e63136de09e02b76
Braulio Bhavamitra <brauliobo@gmail.com>
2021-09-04 16:19:52 -0300

    bot/report: skip message when no data is there

--- bot/report.rb
+++ bot/report.rb
@@ -8,14 +8,15 @@ class Bot
       suffix  = "Scale: #{coin.scale} #{coin.sym} rewarded/#{coin.hr_unit}/24h. TW: count of tracked wallets"
       suffix += "\nOrder: highest #{order} rewards."
       suffix += "\nMultiple days periods are the average of multiple 24h rewards."
-      ds = report_data order
+      data = report_data order
+      return if data.blank?
 
       if report_group? msg
         delete = 3.minutes  if !keep
         delete = 30.minutes if  keep and Time.now.hour % 3 == 0
       end
 
-      send_ds msg, ds, suffix: suffix, delete_both: delete
+      send_ds msg, data, suffix: suffix, delete_both: delete
     end
 
     def report_data order = nil
@@ -32,6 +33,7 @@ class Bot
       end
 
       data = ds.all.map{ |d| SymMash.new d }
+      return if data.blank?
       oc   = order.to_sym if order
       oc   = REPORT_DEFAULT_ORDER unless oc.in? data.first.keys
       data = data.sort{ |a,b| if a[oc] && b[oc] then b[oc] <=> a[oc] elsif a[oc] then -1 else 1 end }
