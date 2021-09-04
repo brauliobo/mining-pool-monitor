8d481d7a632ea712d26a82df230bdcad72b5bc15
Braulio Bhavamitra <brauliobo@gmail.com>
2021-09-04 11:22:37 -0300

    Optimize last pairs with lateral joins

--- db/views.sql
+++ db/views.sql
@@ -13,43 +13,51 @@ drop view if exists wallet_rewards cascade;
 DROP materialized view if exists pairs_materialized cascade;
 
 create or replace view wallet_rewards as
-select 
+select
   r.*,
   balance - lag(balance) over (partition by r.pool, r.wallet order by r.read_at) as reward
-from wallet_reads r 
+from wallet_reads r
+WHERE balance >= 0 --f2pool has a lot of negative balances
 order by r.coin, r.pool, r.wallet, r.read_at desc;
 
-create or replace view ordered_pairs_to_update as
+create or replace view last_pairs_to_update as
 select
-  row_number() over(
-    partition by r.coin, r.pool, r.wallet, i.seq
-    order by r2.read_at DESC, abs(extract(epoch from r2.read_at - r.read_at) / 3600 / 24 - 1) asc
-  ) as row,
-  r.coin, r.pool, r.wallet, 24 as period, i.seq as iseq,
-  extract(epoch from r2.read_at - r.read_at) / 3600 as hours,
-  r.hashrate  AS first_hashrate,
-  r2.hashrate AS second_hashrate,
-  r.balance   AS first_balance,
-  r2.balance  AS second_balance,
-  r.read_at   AS first_read,
-  r2.read_at  AS second_read
-from wallet_reads r
-JOIN wallets_tracked t ON t.coin = r.coin and t.pool = r.pool AND t.wallet = r.wallet AND t.hashrate_last > 0 AND t.last_read_at >= now() - '24 hours'::interval
-join wallet_reads r2 on r2.coin = r.coin and r2.pool = r.pool and r2.wallet = r.wallet
-join intervals i on r.read_at::date = i.start_date and r2.read_at::date = i.end_date
- and 100*abs(extract(epoch from r2.read_at - r.read_at) / 3600 / 24 - 1) < 50
-where (r.pair_24h->'last')::boolean IS NULL;
+  rs.coin, rs.pool, rs.wallet, 24 as period, i.seq as iseq,
+  extract(epoch from rs.read_at - rf.read_at) / 3600 as hours,
+  rf.hashrate AS first_hashrate,
+  rs.hashrate AS second_hashrate,
+  rf.balance  AS first_balance,
+  rs.balance  AS second_balance,
+  rf.read_at  AS first_read,
+  rs.read_at  AS second_read
+from wallets_tracked t, intervals i
+join lateral (
+  select * from wallet_reads rs
+  where t.coin = rs.coin and t.pool = rs.pool AND t.wallet = rs.wallet and rs.read_at::date = i.end_date
+    AND balance > 0
+  order by rs.read_at desc
+  limit 1
+) as rs on true
+join lateral (
+  select * from wallet_reads rf
+  where rf.coin = rs.coin and rf.pool = rs.pool and rf.wallet = rs.wallet and rf.read_at::date = i.start_date
+    AND balance > 0
+  order by abs(extract(epoch from rs.read_at - rf.read_at) / 3600 / 24 - 1) asc
+  limit 1
+) as rf on true
+where t.hashrate_last > 0 AND t.last_read_at >= now() - '24 hours'::interval ;
+--  and (rs.pair_24h->'last')::boolean <> TRUE;
 
 create or replace view pairs_to_update as
 select
   r.*,
   avg(wr.hashrate) as avg_hashrate,
   sum(wr.reward) as reward
-from ordered_pairs_to_update r
+from last_pairs_to_update r
 join wallet_rewards wr on wr.coin = r.coin and wr.pool = r.pool and wr.wallet = r.wallet
- and wr.read_at >= r.first_read and wr.read_at <= r.second_read and reward > -0.02 -- some pools' balances go down
-where row = 1 
-group by 1,2,3,4,5,6,7,8,9,10,11,12,13
+ and wr.read_at >= r.first_read and wr.read_at <= r.second_read
+ and reward > -0.02 -- some pools' balances go down
+group by 1,2,3,4,5,6,7,8,9,10,11,12
 order by iseq, hours desc, second_read DESC;
 
 
@@ -63,14 +71,15 @@ BEGIN
   update wallet_reads
   set pair_24h = json_build_object('{last}', 'null')
   where (pair_24h->'last')::boolean IS TRUE;
-    
+
   -- set new last
-  update wallet_reads r     
+  update wallet_reads r
   set pair_24h = json_build_object('last', true, 'hours', hours, 'reward', reward, 'avg_hashrate', avg_hashrate, 'hashrate', first_hashrate, 'balance', first_balance, 'read_at', first_read)
   from (select * from pairs_to_update) p
-  where r.coin = p.coin and r.pool = p.pool and r.wallet = p.wallet and r.read_at = p.second_read and (pair_24h->'last')::boolean IS NULL;
+  where r.coin = p.coin and r.pool = p.pool and r.wallet = p.wallet and r.read_at = p.second_read
+    and (pair_24h->'last')::boolean IS NULL;
   GET DIAGNOSTICS count = ROW_COUNT;
- 
+
   -- set others to false for faster ordered_pairs_to_update
   update wallet_reads
   set pair_24h = json_build_object('{last}', 'false')
@@ -79,7 +88,7 @@ BEGIN
 return count;
 end;
 $$;
-    
+
 SELECT update_last_readings();
 
 create or replace view pairs_parsed as
@@ -96,17 +105,17 @@ select
 from wallet_reads wp
 JOIN coins c ON c.coin = wp.coin
 join intervals i on read_at::date = i.end_date
-WHERE (pair_24h->'last')::boolean IS TRUE 
+WHERE (pair_24h->'last')::boolean IS TRUE
   AND (pair_24h->'avg_hashrate')::float > 0 AND (pair_24h->'reward')::float >= 0
   AND 100*abs(hashrate / (pair_24h->'avg_hashrate')::float - 1) < 10;
 
 create materialized view pairs_materialized as select * from pairs_parsed;
 
 create or replace view grouped_periods as
-select 
+select
   pid.coin, pid.pool, pid.wallet, id.period,
-  --avg(pid.eth_mh_day) AS eth_mh_day,
-  percentile_cont(0.5) WITHIN GROUP (ORDER by pid.eth_mh_day) as eth_mh_day,
+  avg(pid.eth_mh_day) AS eth_mh_day,
+  --percentile_cont(0.5) WITHIN GROUP (ORDER by pid.eth_mh_day) as eth_mh_day,
   avg(pid."MH") as hashrate,
   sum(pid.hours) as hours,
   sum(pid.reward) as reward,
@@ -122,13 +131,13 @@ order by pid.coin, pid.pool, pid.wallet, id.period;
 create or replace view rewards as
 select
   coin, pool, wallet, b.period,
-  --avg(eth_mh_day) AS eth_mh_day
-  percentile_cont(0.5) WITHIN GROUP (ORDER by eth_mh_day) as eth_mh_day
+  avg(eth_mh_day) AS eth_mh_day
+  --percentile_cont(0.5) WITHIN GROUP (ORDER by eth_mh_day) as eth_mh_day
 from grouped_periods b
 join intervals_defs id on id.period = b.period
 -- for multiple periods consider data starting at least on 2/3 and a minimum of half data points
 where (iseq_max = 1 AND b.period = 24) OR (iseq_max >= round(id.seq * 2/3) and iseq_count >= round(id.seq / 2))
-group by coin, pool, wallet, b.period 
+group by coin, pool, wallet, b.period
 order by coin, pool, wallet, b.period;
 
 
