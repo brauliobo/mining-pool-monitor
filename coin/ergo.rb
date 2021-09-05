d7cfe1d36ac12073fefb1827f339aedf38c8a7b6
Braulio Bhavamitra <brauliobo@gmail.com>
2021-09-05 15:57:02 -0300

    ergo: support herominers

--- coin/ergo.rb
+++ coin/ergo.rb
@@ -12,5 +12,30 @@ module Coin
     self.pools['2miners'].url.gsub! 'eth', 'erg'
     self.pools['2miners'].api.gsub! 'eth', 'erg'
 
+    self.pools.merge!(
+      woolypooly: {
+        url:  'https://woolypooly.com/en/coin/erg/wallet/%{w}',
+        api:  'https://api.woolypooly.com/api/ergo-1/accounts/%{w}',
+        read: -> i {
+          data = get i.api, w: i.wallet
+          SymMash.new(
+            balance:  data.stats.balance,
+            hashrate: stats.reportedHashrate.to_i / 1.0e6,
+          )
+        },
+      },
+      herominers: {
+        url:  'https://ergo.herominers.com/?mining_address=%{w}',
+        api:  'https://ergo.herominers.com/api/stats_address?address=%{w}&recentBlocksAmount=20&longpoll=false',
+        read: -> i {
+          data = get i.api, w: i.wallet
+          SymMash.new(
+            balance:  data.stats.balance.to_i / 1.0e9,
+            hashrate: data.workers.sum{ |w| w.hashrate_24h }.to_i / 1.0e6,
+          )
+        },
+      },
+    )
+
   end
 end
