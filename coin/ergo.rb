4859c4e2e7df22f0d24dddf704204a87a3d33b34
Braulio Bhavamitra <brauliobo@gmail.com>
2021-09-05 09:52:38 -0300

    ergo: add 2miners

--- coin/ergo.rb
+++ coin/ergo.rb
@@ -8,5 +8,9 @@ module Coin
     self.pools.nanopool.url.gsub! 'eth', 'ergo'
     self.pools.nanopool.api.gsub! 'eth', 'ergo'
 
+    self.pools['2miners'] = Eth.pools['2miners'].deep_dup
+    self.pools['2miners'].url.gsub! 'eth', 'erg'
+    self.pools['2miners'].api.gsub! 'eth', 'erg'
+
   end
 end
