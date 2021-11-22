2b3ed79ed56cd2d20c00292c991a6509590683c0
Braulio Bhavamitra <brauliobo@gmail.com>
2021-11-22 15:21:06 -0300

    command: fix typo on change

--- bot/command.rb
+++ bot/command.rb
@@ -25,15 +25,15 @@ class Bot
         help: '<pool> <wallet>',
       },
       pool_wallets: {
-        args: /([w-]+) *(\d*)/,
+        args: /([\w-]+) *(\d*)/,
         help: '<pool> <offset> - List of tracked wallets',
       },
       pool_rewards: {
-        args: /([w-]+) *(\d*)/,
+        args: /([\w-]+) *(\d*)/,
         help: -> { "<pool> <period=(#{DB[:intervals_defs].select_map(:period).join('|')})>" },
       },
       pool_readings: {
-        args: /([w-]+) *(\d*)/,
+        args: /([\w-]+) *(\d*)/,
         help: '<pool> <offset>',
       },
       wallet_rewards: {
