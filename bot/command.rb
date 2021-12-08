63b863d5e46ab8908f184731a23b6f780c0d5159
Braulio Bhavamitra <brauliobo@gmail.com>
2021-12-08 13:10:25 -0300

    bot/wallet_readings: revert limit due to telegram limit

--- bot/command.rb
+++ bot/command.rb
@@ -159,7 +159,7 @@ EOS
         .where(Sequel.ilike :wallet, w)
         .order(Sequel.desc(:read_at), Sequel.asc(:pool))
         .offset(off&.to_i)
-        .limit(25)
+        .limit(20)
       send_ds msg, ds
     end
 
