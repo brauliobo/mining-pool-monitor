191b44cab2595e1475e275bf483f5352096f521d
Braulio Bhavamitra <brauliobo@gmail.com>
2022-03-04 16:47:57 -0300

    etc: support cruxpool

--- coin/etc.rb
+++ coin/etc.rb
@@ -4,10 +4,16 @@ module Coin
     self.sym = name.upcase
 
     self.pools = Eth.pools.deep_dup
+
     self.pools.zetpool.url.gsub! 'eth', 'etc'
     self.pools.zetpool.api.gsub! 'eth', 'etc'
+
     self.pools.crazypool.url.gsub! 'eth', 'etc'
     self.pools.crazypool.api.gsub! 'eth', 'etc'
+
+    self.pools.cruxpool.url.gsub! 'eth', 'etc'
+    self.pools.cruxpool.hashrate.gsub! 'eth', 'etc'
+    self.pools.cruxpool.balance.gsub!  'eth', 'etc'
 
     self.pools.flypool = Eth.pools.ethermine.merge(
       url:  'https://etc.ethermine.org/miners/%{w}',
