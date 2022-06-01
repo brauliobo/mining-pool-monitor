c61693831d1885695dcc0bd7823205421dcbbb60
Braulio Bhavamitra <brauliobo@gmail.com>
2022-06-01 17:25:06 -0300

    tdlib: fix typo

--- tdlib/helpers.rb
+++ tdlib/helpers.rb
@@ -2,7 +2,7 @@ module Tdlib
   module Helpers
 
     def get_supergroup_members supergroup_id: ENV['REPORT_SUPERGROUP_ID']&.to_i, chat_id: ENV['REPORT_CHAT_ID']&.to_i, limit: 200
-      supergroup_id ||= td.get_chat(chat_id: chat_id).value.type.supergroup_i
+      supergroup_id ||= td.get_chat(chat_id: chat_id).value.type.supergroup_id
 
       total = td.get_supergroup_members(supergroup_id: supergroup_id, filter: nil, offset: 0, limit: 1).value.total_count
       pages = (total.to_f / limit).ceil
